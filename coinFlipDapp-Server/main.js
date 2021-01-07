fbd297cac79e05965f52294e101e56440b188b09
Suveett <75737576+Suveett@users.noreply.github.com>
2021-01-07 22:04:26 +0530

    Update main.js

--- coinFlipDapp-Server/main.js
+++ coinFlipDapp-Server/main.js
@@ -1,82 +1,40 @@
-var web3 = new Web3(Web3.givenProvider);
+
+   var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
 
 $(document).ready(function(){
   window.ethereum.enable().then(function(accounts){
-    contractInstance = new web3.eth.Contract(abi, "0x340c4A19652675fFf90C25Ba3C711a5a499b86b3", {from : accounts[0]});
+    contractInstance = new web3.eth.Contract(abi, web3.utils.toChecksumAddress("0xB810CAd9759F2CdbDcd9ec0d527000db51e70B70"), {from : accounts[0]});
     console.log(contractInstance);
     });
 
-    var config1 = {
-      value : web3.utils.toWei("0.5", "ether")
+  
+    $("#flip_button").click(flipCoin);
+  
+  
+    
+    function flipCoin(){
+        var bet = $("#bet_input").val();
+        var config = {
+            value: web3.utils.toWei(bet,"ether")
+        }
+        contractInstance.methods.flipCoin().send(config)
+        .on("transactionHash", function(hash){
+            console.log(hash);
+        })
+        .on("confirmation", function(confirmationNr){
+            console.log(confirmationNr);
+        })
+        .on("receipt", function(receipt){
+            console.log(receipt);
+            if(receipt.events.betPlaced.returnValues[2] === false){
+                alert("You lost " + bet + " Ether!");
+            }
+            else if(receipt.events.betPlaced.returnValues[2] === true){
+                alert("You won " + bet + " Ether!");
+            }
+        })
+
+
     }
-    var config2 = {
-      value : web3.utils.toWei("1", "ether")
-    }
-
-    var config3 = {
-      value : web3.utils.toWei("2", "ether")
-    }
-
-
-
-    $("#open-door1").click(function(){
-      contractInstance.methods.flipCoin().send(config1)
-      .on("transactionHash", function(hash){
-        console.log(hash);
-      })
-      .on("confirmation", function(confirmationNr){
-        console.log(confirmationNr);
-      })
-      .on("receipt", function(receipt){
-        console.log(receipt);
-        alert("Funds received!");
-        if(receipt.events.betPlaced.returnValues[2] === false){
-            alert("You lost " +  " 0.5 Ether!");
-        }
-        else if(receipt.events.betPlaced.returnValues[2] === true){
-            alert("You won "  + " 0.5 Ether!");
-        }
-      });
-
-    });
-
-    $("#open-door2").click(function(){
-      contractInstance.methods.flipCoin().send(config2)
-      .on("transactionHash", function(hash){
-        console.log(hash);
-      })
-      .on("confirmation", function(confirmationNr){
-        console.log(confirmationNr);
-      })
-      .on("receipt", function(receipt){
-        console.log(receipt);
-        alert("Funds received!");
-        if(receipt.events.betPlaced.returnValues[2] === false){
-            alert("You lost "  + " 1 Ether!");
-        }
-        else if(receipt.events.betPlaced.returnValues[2] === true){
-            alert("You won "  + " 1 Ether!");
-        }
-      })
-    });
-
-    $("#open-door3").click(function(){
-      contractInstance.methods.flipCoin().send(config3)
-      .on("transactionHash", function(hash){
-        console.log(hash);
-      })
-      .on("confirmation", function(confirmationNr){
-        console.log(confirmationNr);
-      })
-      .on("receipt", function(receipt){
-        console.log(receipt);
-        if(receipt.events.betPlaced.returnValues[2] === false){
-            alert("You lost " + " 2 Ether!");
-        }
-        else if(receipt.events.betPlaced.returnValues[2] === true){
-            alert("You won "  + " 2 Ether!");
-        }
-      })
-    });
 });
